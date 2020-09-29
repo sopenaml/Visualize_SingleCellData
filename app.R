5eb1e06855da36468aa4c2de66810d40a7723c7d
sopenaml <miriam.lloriansopena@crick.ac.uk>
2020-09-29 16:30:35 +0100

    Update app.R
    Include an option to upload a file with a gene list, gene signature. The list is used in AddModuleScore, and the values are plotted in a FeaturePlot

--- app.R
+++ app.R
@@ -10,9 +10,10 @@ library(shiny)
 # the app.R and the seuratObject.RDS  should be copied to  www/shiny/bioinformatician/project_dir
 
 
-scefilt.toplot <- readRDS( "PATH_TO_SEURAT_OBJECT.RDS")
-allGenes <- rownames(scefilt.toplot)
+#scefilt.toplot <- readRDS( "PATH_TO_SEURAT_OBJECT.RDS")
+scefilt.toplot <- readRDS("integrated.data.filt.clstrs.cc_corrected.rds")
 
+allGenes <- rownames(scefilt.toplot)
 
 ################################################################
 ## this code chunk splits the page in panels 
@@ -22,44 +23,75 @@ allGenes <- rownames(scefilt.toplot)
 
 ui <- fluidPage(
   # This is the main title for the app
-titlePanel ( "PROJECT_NAME"),  
- # textInput(inputId = "Gene",
+  
+  titlePanel ( "TESTING"),
+  # this creates a box where scientist can input their gene of interest
+  # 
+  # textInput(inputId = "Gene",
   #           label = "Gene Symbol:",
   #           value = "Myl3" ),
   # 
   ## this allows for entering multiple genes 
   selectizeInput(inputId = "genes",
-                 label = "Enter Genes to plot:",
+                 label = "Enter/Select Genes to plot:",
                  choices = c(as.vector(sort(unique(allGenes)))),
                  selected = c("Myl3","Myl2", "Myl7"),
                  multiple = TRUE,
                  options = NULL ),
-
-  # Top 2 panels 
+  # Upload file
+  fileInput(
+    inputId = "File1",
+    label = "Upload File to use as gene signature",
+    multiple = FALSE,
+    accept = c("text/csv",
+               "text/comma-separated-values,text/plain",
+               ".csv"),
+    width = NULL,
+    buttonLabel = "Browse...",
+    placeholder = "No file selected"
+  ),
+  # give list a name to appear in the Feature Plot
+  textInput(inputId = "GeneListName",
+            label = "Name Gene Signature",
+            value = "" ),
+  
+  # Top 2 panels
   fluidRow(
     # left panel is a static view of UMAP generated for the analysis
-    column(4,
+    column(5,
            "TSNE Plot",
            plotOutput('tsne')
-           ),
+    ),
     ## Panel for features Plot, responds to change in input Gene box
     column(6,
            "Features Plot",
            downloadButton("downloadPlot", "Download"),
            plotOutput('plot1')
-        )
-    ),
-  # bottom panel
+    )
+  ),
+  # middle panel
   fluidRow(
     ## Panel for Violin Plot, responds to change in input Gene box
     column( 8,
             "Violin Plot",
             downloadButton("downloadVPlot", "Download"),
             plotOutput('plot2')
-           )
-   )
+    ),
+    column(4,
+           "Features Plot for Gene Signatures",
+           downloadButton("downloadPlot3", "Download"),
+           plotOutput('plot3')
+    )
+  ),
+  # bottom panel
+  fluidRow(
+    ## Panel for DotPlot
+    column( 8,
+            "DotPlot",
+            downloadButton("downloadDPlot", "Download"),
+            plotOutput('dotplot'))
+  )
 )
-
 ##########################################################
 ## this is the code that reads data and generates plots ##
 ## for static plots you just create an output
@@ -72,23 +104,27 @@ titlePanel ( "PROJECT_NAME"),
 
 server <- function(input, output) {
   
- # Plot static UMAP/TSNE only uses renderPlot
+  # Plot static UMAP/TSNE only uses renderPlot
   
   output$tsne <- renderPlot({ 
     DimPlot(scefilt.toplot, reduction = "umap")
     
-   
   }) 
   
-## Features plot responds to input Gene box
 
-# create plot
-    geneplot <- reactive ({ 
-      FeaturePlot(object = scefilt.toplot, 
-      features = input$Gene)
-
-   })
+  
+  ## Features plot responds to input Gene box
+  
+  geneplot <- reactive ({ 
     
+    FeaturePlot(object = scefilt.toplot, 
+                #features = input$Gene)
+                features = input$genes)
+    
+  })
+ 
+   
+
 # print plot
   output$plot1 <- renderPlot({
     print(geneplot())
@@ -102,11 +138,15 @@ output$downloadPlot <- downloadHandler(
     }
   )
 
+
 ## Violin plot responds to input Gene box
 # create plot
   Vlnplot <- reactive ({
      VlnPlot(object = scefilt.toplot, 
-                        features = input$Gene )
+             #features = input$Gene)
+             features = input$genes,
+             ncol=2)
+    
 })
 
 # print plot
@@ -123,7 +163,76 @@ output$downloadPlot <- downloadHandler(
     }
   )
  
-    
+ 
+ 
+ ### Features Plot for Gene signatures
+ 
+ 
+ # # Read in file with list of genes
+ # data1 <- reactive({
+ #   req(input$File1)
+ #      inFile <- input$File1
+ #      read.csv(inFile$datapath, stringsAsFactors = FALSE)  
+ #   
+ #   
+ # })
+ # create plot
+
+ genesignplot <- reactive ({ 
+   req(input$File1)
+   inFile <- input$File1
+   data1 <- read.csv(inFile$datapath, stringsAsFactors = FALSE)
+   
+   scefilt.toplot <- AddModuleScore(object= scefilt.toplot,
+                                    features=  data1,
+                                    name= input$GeneListName,
+                                    ctrl= length(data1))
+   
+   FeaturePlot(object = scefilt.toplot, 
+               #features = input$Gene)
+               features = paste0(input$GeneListName,"1"))   
+ }) 
+
+ 
+ output$plot3 <- renderPlot({
+   
+   print(genesignplot())
+   
+ })
+ # download feature 
+ output$downloadPlot3 <- downloadHandler(
+   filename = function() { paste(input$GeneListName, '.png', sep='') },
+   content = function(file) {
+     ggsave(file,genesignplot(),device = "png")
+   }
+ )
+ 
+ ## Features plot responds to multiple gene entry
+ # 
+ # # create plot
+ Dotplot <- reactive ({
+   DotPlot(object = scefilt.toplot,
+               features = input$genes)
+           #cols = c("blue", "red", "green"), 
+          # split.by = "genotype")  
+             
+
+
+ })
+ # 
+ # # print plot
+ output$dotplot <- renderPlot({
+   print(Dotplot())
+ })
+ # 
+ # # download feature
+ # output$downloadPlot <- downloadHandler(
+ #   filename = function() { paste(input$Gene, '.png', sep='') },
+ #   content = function(file) {
+ #     ggsave(file,Dotplot(),device = "png")
+ #   }
+ # )
+ # 
   
 }
 
